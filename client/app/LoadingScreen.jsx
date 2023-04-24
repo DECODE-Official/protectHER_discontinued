735659d1a1ca393d837bbd3bf9801185e05491da
void main() <100292340+Webwizards60@users.noreply.github.com>
2023-04-24 23:19:26 +0530

    :sparkles: Added Login Page

--- client/app/LoadingScreen.jsx
+++ client/app/LoadingScreen.jsx
@@ -1,10 +1,19 @@
 import {View, Text, SafeAreaView, Image} from 'react-native';
 import styles from '../Styles/LoadingScreenStyle';
+import icon from '../assets/icon.png';
+import LottieView from "lottie-react-native";
 
 const LoadingScreen = () => {
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text} >Hello world</Text>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={icon}/>
+            </View>
+            <Text style={styles.text}>protectHER</Text>
+            <LottieView source={require('../json/loading.json')} autoPlay loop
+                                style={{height: 100, width: 100}}/>
+            <Text>Loading...</Text>
+            <Text style={styles.BottomText}>Ensure her Protection with just a tap</Text>
         </SafeAreaView>
     )
 }
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,9 +1,15 @@
 import {View, Text, SafeAreaView } from 'react-native';
 import {Stack, useRouter} from "expo-router";
 import LoadingScreen from "./LoadingScreen";
+import {useState} from "react";
+import Login from '../BeforeAuth/login'
 
 const Home = () => {
     const router = useRouter();
+    const [loading, setLoading] = useState(true);
+    setTimeout(() => {
+        setLoading(false)
+    },3000)
   return (
     <SafeAreaView>
         <Stack.Screen style={{backgroundColor: "#fff", flex: 1}}
@@ -12,7 +18,9 @@ const Home = () => {
                 headerShown: false,
             }}
         />
-        <LoadingScreen/>
+        {
+            loading ? <LoadingScreen/>: <Login/>
+        }
     </SafeAreaView>
   )
 }
