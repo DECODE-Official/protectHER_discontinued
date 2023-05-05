c58a895c99a8e54c1b4dcb01c293317eb7739432
void main() <sourabhsk.1812@gmail.com>
2023-05-05 11:28:19 +0530

    :construction: Setting up formik for Form validation

--- client/app/auth/register.js
+++ client/app/auth/register.js
@@ -12,44 +12,6 @@ const register = () => {
     let theme = useColorScheme();
     let Theme = theme === 'light' ? LIGHT : DARK;
 
-    // Formik Hook
-    const {handleChange, handleBlur, handleSubmit, values} = useFormik({
-        initialValues: {
-            email: '', phone: '', password: '', confirmPassword: ''
-        }
-    })
-
-
-    // Input array to create TextInput
-    const Input = [{
-        placeholder: "Email",
-        secureTextEntry: false,
-        _id: 1,
-        value: values.email,
-        type: 'email-address',
-        name: 'email'
-    }, {
-        placeholder: "Phone No.",
-        secureTextEntry: false,
-        _id: 2,
-        value: values.phone,
-        type: 'phone-pad',
-        name: 'phone'
-    }, {
-        placeholder: "Create Password",
-        secureTextEntry: true,
-        _id: 3,
-        value: values.password,
-        type: 'default',
-        name: 'password'
-    }, {
-        placeholder: "Confirm Password",
-        secureTextEntry: true,
-        _id: 4,
-        value: values.confirmPassword,
-        type: 'default',
-        name: 'confirmPassword'
-    }]
 
     return (<ScrollView style={Styles.view} contentContainerStyle={{
         justifyContent: 'center', height: '100%', backgroundColor: Theme.background
@@ -65,16 +27,32 @@ const register = () => {
                 {({handleChange, handleBlur, handleSubmit, values}) => (
                     <View style={Styles.Form}>
                         <View style={Styles.inputArea}>
-                            {// Mapping the Input array to create TextInput for each item
-                                Input.map((item) => {
-                                    return (<TextInput key={item._id} style={[Styles.input, {
-                                        backgroundColor: Theme.input_Background, color: Theme.input_Color
-                                    }]} placeholderTextColor='#818181' placeholder={item.placeholder} value={item.value}
-                                                       onChangeText={handleChange(item.name)} id={item._id} name={item.name}
-                                                       keyboardType={item.type}
-                                                       onBlur={handleBlur(item.name)}
-                                                       secureTextEntry={item.secureTextEntry}/>)
-                                })}
+                            <TextInput style={[Styles.input, {
+                                backgroundColor: Theme.input_Background,
+                                color: Theme.input_Color
+                            }]} placeholder='Email'
+                                       placeholderTextColor={Theme.secondary_Text} onChangeText={handleChange('email')}
+                                       onBlur={handleBlur('email')} value={values.email}/>
+                            <TextInput style={[Styles.input, {
+                                backgroundColor: Theme.input_Background,
+                                color: Theme.input_Color
+                            }]} placeholder='Phone'
+                                       placeholderTextColor={Theme.secondary_Text} onChangeText={handleChange('phone')}
+                                       onBlur={handleBlur('phone')} value={values.phone}/>
+                            <TextInput style={[Styles.input, {
+                                backgroundColor: Theme.input_Background,
+                                color: Theme.input_Color
+                            }]} placeholder='Create Password'
+                                       placeholderTextColor={Theme.secondary_Text}
+                                       onChangeText={handleChange('password')} secureTextEntry={true}
+                                       onBlur={handleBlur('password')} value={values.password}/>
+                            <TextInput style={[Styles.input, {
+                                backgroundColor: Theme.input_Background,
+                                color: Theme.input_Color
+                            }]} placeholder='Comfirm Password'
+                                       placeholderTextColor={Theme.secondary_Text}
+                                       onChangeText={handleChange('confirm password')}
+                                       onBlur={handleBlur('confirm password')} value={values.confirmPassword}/>
                         </View>
                         <TouchableOpacity style={Styles.register} onPress={handleSubmit}>
                             <LinearGradient colors={['#5fcfff', '#00a0e5']} style={Styles.registerButton}>
